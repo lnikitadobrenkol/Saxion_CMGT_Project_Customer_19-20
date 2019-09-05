cac500a82d0f01659170317500b142c315178ec8
lnikitadobrenkol <ldobrenkonikital@gmail.com>
2019-09-05 19:38:43 +0200

    Score and complexity controller
    - Score system
    - Complexity system
    - Test UI components

---
+++ JustDrive/Assets/Scripts/ScoreAndComlexityController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreAndComlexityController : MonoBehaviour
+{
+    public Text scoreText;
+    //public DeathMenuController deathMenu;
+
+    private float score = 0.0f;
+
+    private int difficultyLevel = 1;
+    private int maxDifficultyLevel = 10;
+    private int scoreToNextLevel = 10;
+
+    private bool isDead = false;
+
+    void Update()
+    {
+        if (isDead)
+        {
+            return;
+        }
+
+        if (score >= scoreToNextLevel)
+        {
+            LevelUp();
+        }
+        score += Time.deltaTime * difficultyLevel;
+        scoreText.text = ((int)score).ToString();
+    }
+
+    void LevelUp()
+    {
+        if (difficultyLevel == maxDifficultyLevel)
+        {
+            return;
+        }
+
+        scoreToNextLevel *= 2;
+        difficultyLevel++;
+
+        GetComponent<PlayerController>().SetSpeed(difficultyLevel);
+    }
+
+    public void OnDeath()
+    {
+        isDead = true;
+        //deathMenu.ToggleEndMenu(score);
+    }
+}
