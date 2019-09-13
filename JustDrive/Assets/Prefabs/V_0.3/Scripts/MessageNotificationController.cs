77fa084155a2b6f4a44e7e7e2d45d9c990299f65
lnikitadobrenkol <ldobrenkonikital@gmail.com>
2019-09-13 22:11:57 +0200

    NotificationButton

---
+++ JustDrive/Assets/Prefabs/V_0.3/Scripts/MessageNotificationController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MessageNotificationController : MonoBehaviour
+{
+    public GameObject messageNotification;
+
+    private bool isShown;
+
+    void Start()
+    {
+        isShown = false;
+        messageNotification.SetActive(false);
+
+        InvokeRepeating("ShowNotification", 2.0f, 2.0f);
+    }
+
+    private void ShowNotification()
+    {
+        if (isShown)
+        {
+            messageNotification.SetActive(false);
+            isShown = false;
+        }
+        else
+        {
+            messageNotification.SetActive(true);
+            isShown = true;
+        }
+    }
+}
