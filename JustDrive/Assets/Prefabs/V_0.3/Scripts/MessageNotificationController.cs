ee7bbb8bcababc61d82b15152678952180031470
lnikitadobrenkol <ldobrenkonikital@gmail.com>
2019-09-14 16:18:14 +0200

    Open/Hide message menu
    - Add the possobility to open and close message menu with the wanted behaviour
    - Add the possobility to open and close message menu using keyboard
    - Small refactoring for more clean code

--- JustDrive/Assets/Prefabs/V_0.3/Scripts/MessageNotificationController.cs
+++ JustDrive/Assets/Prefabs/V_0.3/Scripts/MessageNotificationController.cs
@@ -1,32 +1,108 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
+// This class controlls how offten message notification appears on the screen and when diappears.
+// This class has methods to open and close message menu witch are attached to notification button and close button in the message menu.
 public class MessageNotificationController : MonoBehaviour
 {
     public GameObject messageNotification;
+    public GameObject messageMenu;
 
-    private bool isShown;
+    public float frequencyOfOccurrence = 2.0f;
+
+    private bool isNotificationShown;
+    private bool isMessageMenuOpen;
+
+    private bool canOpenMessageMenu;
+    private bool canCloseMessageMenu;
 
     void Start()
     {
-        isShown = false;
         messageNotification.SetActive(false);
+        messageMenu.SetActive(false);
 
-        InvokeRepeating("ShowNotification", 2.0f, 2.0f);
+        isNotificationShown = false;
+        isMessageMenuOpen = false;
+
+        InvokeRepeating("ShowNotification", 3.0f, frequencyOfOccurrence); // call the (method, in n seconds, every n seconds) 
+    }
+
+    private void Update()
+    {
+        // Open menu by pushing 'R'
+        if (CanOpenMessageMenu())
+        {
+            OpenMessageMenu();
+        }
+
+        // Close menu by pushing 'Esc'
+        if (CanCloseMessageMenu())
+        {
+            CloseMessageMenu();
+        }
+    }
+
+    // Attached to notification button
+    public void OpenMessageMenu()
+    {
+            messageMenu.SetActive(true);
+            isMessageMenuOpen = true;
+
+            messageNotification.SetActive(false); // I do not want to see new notification while I am reading messages
+    }
+
+    // Attached to the close button in the message menu
+    public void CloseMessageMenu()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            messageMenu.SetActive(false);
+            isMessageMenuOpen = false;
+
+            StartCoroutine(Delay(frequencyOfOccurrence)); // I do not want to see notification at once I closed the message menu
+        }
     }
 
     private void ShowNotification()
     {
-        if (isShown)
+        if (isNotificationShown || isMessageMenuOpen)
         {
             messageNotification.SetActive(false);
-            isShown = false;
+            isNotificationShown = false;
         }
         else
         {
             messageNotification.SetActive(true);
-            isShown = true;
+            isNotificationShown = true;
         }
+    }
+
+    private bool CanOpenMessageMenu()
+    {
+        if (Input.GetKeyDown(KeyCode.R) && isNotificationShown)
+        {
+            return true;
+        } 
+        else
+        {
+            return false;
+        }
+    }
+
+    private bool CanCloseMessageMenu()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) && isMessageMenuOpen)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    IEnumerator Delay(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
     }
 }
