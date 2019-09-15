1f5fbf0bc16de36bc26e35abe6ae814c91ff2e8e
lnikitadobrenkol <ldobrenkonikital@gmail.com>
2019-09-15 16:06:44 +0200

    Create a separated quiz game
    - Create a UI for quiz game
    - Create a system for quiz game (base for messaging system)
    - Create an editor for questions (means UI and script to edit question)

---
+++ JustDrive/Assets/Prefabs/V_0.3/Scripts/DataController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.IO;
+
+public class DataController : MonoBehaviour
+{
+    private RoundData[] allRoundData;
+
+    private string gameDataFileName = "data.json";
+
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+
+        LoadGameData();
+
+        SceneManager.LoadScene("MenuScreen");
+    }
+
+    public RoundData GetCurrentRoundData()
+    {
+        return allRoundData[0];
+    }
+
+    private void LoadGameData()
+    {
+        string filePath = Path.Combine(Application.streamingAssetsPath, gameDataFileName);
+
+        if (File.Exists(filePath))
+        {
+            string dataAsJSON = File.ReadAllText(filePath);
+            GameData loadedData = JsonUtility.FromJson<GameData>(dataAsJSON);
+
+            allRoundData = loadedData.allRoundData;
+        }
+        else
+        {
+            Debug.LogError("Can not load game data!");
+        }
+    }
+}
---
+++ JustDrive/Assets/Prefabs/V_0.3/Scripts/Question.cs
@@ -0,0 +1,6 @@
+﻿[System.Serializable]
+public class Question
+{
+    public string question;
+   // public string correctAnswer, answer1, answer2;
+}
