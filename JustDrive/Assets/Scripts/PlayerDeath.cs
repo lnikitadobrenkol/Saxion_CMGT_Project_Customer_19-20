4ac34fc9ed3ecfb96e6c87d03ff774bfc058f2b1
lnikitadobrenkol <ldobrenkonikital@gmail.com>
2019-09-17 14:54:35 +0200

    Car-obstacle system
    - Obstacles are being spawned. Developers can easily control it with out tuching the code
    - Player can loose if faces with obstacles
    - Simple behaviour of car obstacles

---
+++ JustDrive/Assets/Scripts/PlayerDeath.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDeath : MonoBehaviour
+{
+    private CharacterController playerController;
+    public GameController endRound;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerController = GetComponent<CharacterController>();
+    }
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if (hit.point.z > transform.position.z + playerController.radius)
+        {
+            endRound.EndRound();
+        }
+    }
+}
